9b2ec9bf8c2745a64bb10f6756dddde6b03c8164
Lev Kupchinov <lev.kupchinov@yandex.ru>
2018-03-16 11:39:25 +0300

    Initial Commit

---
+++ Noosphera-traineeTests/Noosphera_traineeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Noosphera_traineeTests.swift
+//  Noosphera-traineeTests
+//
+//  Created by Лев Купчинов on 16.03.2018.
+//  Copyright © 2018 Лев Купчинов. All rights reserved.
+//
+
+import XCTest
+@testable import Noosphera_trainee
+
+class Noosphera_traineeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
